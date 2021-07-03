286a005b9b5641f5387b31fdd8beef537d763172
ZhouZhipeng <1334093285@qq.com>
2021-07-03 19:03:39 +0800

    add calculator test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+
+
+    public int compute(int i, int i1, char s) {
+        return -1;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Author :13340
+ * @Date :Created in 2021-07-03-18:50
+ * @Description:
+ **/
+
+public class CalculatorTest {
+    @Test
+    public void shouldReturn2When1plus1(){
+        Calculator calculator = new Calculator();
+        int realResult = calculator.compute(1,1,'+');
+        int expectResult = 2;
+        Assert.assertEquals(realResult,expectResult);
+    }
+    @Test
+    public void shouldReturn0When1Sub1(){
+        Calculator calculator = new Calculator();
+        int realResult = calculator.compute(1,1,'-');
+        int expectResult = 0;
+        Assert.assertEquals(realResult,expectResult);
+    }
+
+}
