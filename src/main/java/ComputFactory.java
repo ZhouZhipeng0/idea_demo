daea3a9fee866fde6e30c279fd32d37ebd065d20
ZhouZhipeng <1334093285@qq.com>
2021-07-04 23:26:53 +0800

    refactor calculator use reflect

--- src/main/java/ComputFactory.java
+++ src/main/java/ComputFactory.java
@@ -8,14 +8,7 @@
 public class ComputFactory {
     private static final Sub sub = new Sub();
     private static final Add add = new Add();
-    public static Computable getCompute(char symbol) {
-        switch (symbol) {
-            case '+':
-                return add;
-            case '-':
-                return sub;
-            default:
-                throw new IllegalArgumentException();
-        }
+    public static Computable getCompute(String symbol) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        return (Computable) Class.forName(symbol).newInstance();
     }
 }
